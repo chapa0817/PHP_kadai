7d8f02faec89fa848d43009a176375f049b1e624
chapa0817 <145003120+chapa0817@users.noreply.github.com>
2023-11-11 17:23:04 -0500

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHPの連想配列課題じゃ</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+    $shopping_receipt = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+ 
+    print_r($shopping_receipt);
+
+?>
+</p>
+</body>
+
+</html>
